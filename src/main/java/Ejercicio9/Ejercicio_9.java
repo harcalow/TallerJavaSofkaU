819436756e81aed8d20d6d47ebb594368bc67555
Harold Calderon <harold.1408@hotmail.com>
2021-09-07 18:05:29 -0500

    Se implementa ejercico 9 y 10

--- src/main/java/Ejercicio9/Ejercicio_9.java
+++ src/main/java/Ejercicio9/Ejercicio_9.java
@@ -1,8 +1,12 @@
 package Ejercicio9;
-
-public class Ejercicio_9 {
+public class Ejercicio_9 extends DataEntry.InputData{
     public void run(){
-
+        System.out.println("Ejercicio 9");
+        String text ="La sonrisa sera la mejor arma contra la tristeza ";
+        text=text.replaceAll("a","e");
+        System.out.println("Ingresa frase adicionar ");
+        String inText=intext();
+        text=text+inText;
+        System.out.println(text);
     }
-
 }
