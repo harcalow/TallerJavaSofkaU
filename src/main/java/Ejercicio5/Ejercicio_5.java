d1a28229ead20275003ddf5fbfab98a1a6cd011a
Harold Calderon <harold.1408@hotmail.com>
2021-09-07 16:31:01 -0500

    se implementa ejercicio 5

---
+++ src/main/java/Ejercicio5/Ejercicio_5.java
@@ -0,0 +1,13 @@
+package Ejercicio5;
+public class Ejercicio_5 {
+    public void run(){
+        System.out.println("Numero impares while 0 a 100");
+        int i=0;
+        while (i<=100){
+            if (i%2!=0){
+                System.out.print(i+" ");
+            }
+            i++;
+        }
+    }
+}
--- src/main/java/Ejercicios.java
+++ src/main/java/Ejercicios.java
@@ -2,6 +2,8 @@ import Ejercicio1.Ejercicio_1;
 import Ejercicio2.Ejercicio_2;
 import Ejercicio3.Ejercicio_3;
 import Ejercicio4.Ejercicio_4;
+import Ejercicio5.Ejercicio_5;
+
 public class Ejercicios {
     public void runEjercicio1(){
         Ejercicio_1 ejercicio1=new Ejercicio_1();
@@ -18,6 +20,10 @@ public class Ejercicios {
     public void runEjercicio4(){
         Ejercicio_4 ejercicio4=new Ejercicio_4();
         ejercicio4.run();
+    }
+    public void runEjercicio5(){
+        Ejercicio_5 ejercicio5=new Ejercicio_5();
+        ejercicio5.run();
     }
 
 }
