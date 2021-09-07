5689ab6176db1a40d8393b81b69845dc57a85ba8
Harold Calderon <harold.1408@hotmail.com>
2021-09-07 09:32:02 -0500

    Initial commit Creación de archívo principal main y ejercicio1

---
+++ src/main/java/Ejercicio1/Ejercicio_1.java
@@ -0,0 +1,16 @@
+package Ejercicio1;
+public class Ejercicio_1 {
+        public void run(){
+            double x,y;
+            x=20;
+            y=10;
+            System.out.println("Valores para las pruebas son x: "+x+" y:"+y);
+            if (x>y){
+                System.out.println("x :"+x+" Es mayor que y :"+y);
+            }else if(x<y){
+                System.out.println("x :"+x+" Es menor que y :"+y);
+            }else{
+                System.out.println("x :"+x+" son iguales y :"+y);
+            }
+        }
+}
---
+++ src/main/java/Ejercicios.java
@@ -0,0 +1,7 @@
+import Ejercicio1.Ejercicio_1;
+public class Ejercicios {
+    public void runEjercicio1(){
+        Ejercicio_1 ejercicio1=new Ejercicio_1();
+        ejercicio1.run();
+    }
+}
