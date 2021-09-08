a4524c68d9bd218846bd52db535ee89fc987b302
Harold Calderon <harold.1408@hotmail.com>
2021-09-07 19:45:57 -0500

    Se implementa ejercico 14 y 15

--- src/main/java/Ejercicio14/Ejercicio_14.java
+++ src/main/java/Ejercicio14/Ejercicio_14.java
@@ -1,8 +1,16 @@
 package Ejercicio14;
 
-public class Ejercicio_14 {
-    public void run(){
+import javax.xml.crypto.Data;
 
+public class Ejercicio_14 extends DataEntry.InputData {
+    public void run(){
+        System.out.println("Ejercico 14 \n" +
+                "Ingresa el numero que quieres que empiece a contar en 2 en 2 \n " +
+                "hasta 1000\n");
+        int number=(int) inValue(" numero ");
+        for (int i=number;i<=1000;i+=2){
+            System.out.print(i+" ");
+        }
     }
 
 }
--- src/main/java/Ejercicio15/Ejercicio_15.java
+++ src/main/java/Ejercicio15/Ejercicio_15.java
@@ -1,8 +1,51 @@
 package Ejercicio15;
 
-public class Ejercicio_15 {
+import javax.xml.crypto.Data;
+
+public class Ejercicio_15 extends DataEntry.InputData {
     public void run(){
-
+        System.out.println("Ejercicio 15");
+        boolean exit=true;
+        while(exit) {
+            String menu = "****** GESTION CINEMATOGRAFICA ********\n" +
+                    "1-NUEVO ACTOR\n" +
+                    "2-BUSCAR ACTOR\n" +
+                    "3-ELIMINAR ACTOR\n" +
+                    "4-MODIFICAR ACTOR\n" +
+                    "5-VER TODOS LOS ACTORES\n" +
+                    "6-VER PELICULAS DE LOS ACTORES\n" +
+                    "7-VER CATEGORIA DE LAS PELICULAS DE LOS ACTORES\n" +
+                    "8-SALIR";
+            System.out.println(menu);
+            System.out.println("Selecciona el item");
+            int item = (int) inValue(" item ");
+            if (item == 8) {
+                exit = false;
+            }else if((item>0)&&(item<8)){
+                switch (item){
+                    case (1):
+                        System.out.println("1-NUEVO ACTOR\n");
+                        break;
+                    case (2):
+                        System.out.println("2-BUSCAR ACTOR\n");
+                        break;
+                    case (3):
+                        System.out.println("3-ELIMINAR ACTOR\n");
+                        break;
+                    case (4):
+                        System.out.println("4-MODIFICAR ACTOR\n");
+                        break;
+                    case (5):
+                        System.out.println("5-VER TODOS LOS ACTORES\n");
+                        break;
+                    case (6):
+                        System.out.println("6-VER PELICULAS DE LOS ACTORES\n");
+                        break;
+                    case (7):
+                        System.out.println("7-VER CATEGORIA DE LAS PELICULAS DE LOS ACTORES\n");
+                        break;
+                }
+            }
+        }
     }
-
 }
